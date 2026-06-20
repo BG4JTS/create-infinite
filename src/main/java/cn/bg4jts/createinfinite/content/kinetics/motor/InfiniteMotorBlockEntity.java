808e9dafad5f1ae7e4ff3174a878cae6f30e32dd
BG4JTS <zhaodachuan2@outlook.com>
2026-06-20 09:44:18 +0800

    fix: 应力输出始终为无限，不再依赖输入转速

--- src/main/java/cn/bg4jts/createinfinite/content/kinetics/motor/InfiniteMotorBlockEntity.java
+++ src/main/java/cn/bg4jts/createinfinite/content/kinetics/motor/InfiniteMotorBlockEntity.java
@@ -1,7 +1,6 @@
 package cn.bg4jts.createinfinite.content.kinetics.motor;
 
 import com.simibubi.create.content.kinetics.base.GeneratingKineticBlockEntity;
-import cn.bg4jts.createinfinite.registry.ModBlocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -20,10 +19,7 @@ public class InfiniteMotorBlockEntity extends GeneratingKineticBlockEntity {
 
 	@Override
 	public float calculateAddedStressCapacity() {
-		float speed = Math.abs(this.getSpeed());
-		if (speed == 0)
-			return 0;
-		this.lastCapacityProvided = 0.5f * Float.MAX_VALUE;
+		this.lastCapacityProvided = Float.MAX_VALUE;
 		return this.lastCapacityProvided;
 	}
 
